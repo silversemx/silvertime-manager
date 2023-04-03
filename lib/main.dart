cb8893e113d745db55f2a0be5e44f7d7b2d812ed
Djcharles26 <jlaraanaya@gmail.com>
2023-04-02 21:53:44 -0600

    Updated main tree with ui provider 𐂷

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:silvertime/include.dart';
+import 'package:silvertime/providers/ui.dart';
 import 'package:silvertime/router.dart';
 import 'package:silvertime/screens/splash.dart';
 import 'package:silvertime/style/themes.dart';
@@ -25,6 +26,17 @@ class _SilverTimeState extends State<SilverTime> {
   final GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey();
 
   @override
+  void didChangeDependencies() {
+    precacheImage(const AssetImage ("assets/logos/silvertime.png"), context);
+    precacheImage(const AssetImage ("assets/logos/silvertime0.5.png"), context);
+    precacheImage(const AssetImage ("assets/logos/silvertime_white.png"), context);
+    precacheImage(const AssetImage ("assets/logos/silvertime_white0.5.png"), 
+      context
+    );
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     SystemChrome.setApplicationSwitcherDescription(
       ApplicationSwitcherDescription(
@@ -34,45 +46,48 @@ class _SilverTimeState extends State<SilverTime> {
     );
 
     return MultiProvider(
-      providers: const [],
-      child: Consumer(
-        builder: (ctx, ui, child) {
-          //ui.fetchSettings ();
+      providers: [
+        ChangeNotifierProvider.value(
+          value: UI ()
+        )
+      ],
+      child: Consumer<UI>(
+        builder: (ctx, ui, _) {
+          ui.fetchSettings ();
           //Provider.of<Auth> (context, listen: false).tryAutoLogin ();
-          return child!;
+          return MaterialApp(
+            title: appName,
+            debugShowCheckedModeBanner: runtime == "Test",
+            scaffoldMessengerKey: _scaffoldKey,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: ThemeMode.light,
+            initialRoute: SplashScreen.routeName,
+            navigatorKey: locator<NavigationService> ().navigatorKey,
+            builder: (cx, child) {
+              return ScrollConfiguration(
+                behavior: ScrollClean ().copyWith(
+                  scrollbars: false
+                ), 
+                child: child ?? Container ()
+              );
+            },
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            locale: Locale (ui.locale),
+            onGenerateRoute: (settings) => RouterAdmin.generateRoute (
+              context, settings
+            ),
+            navigatorObservers: [
+              locator<RouteObserver> ()
+            ],
+          );
         }, 
-        child: MaterialApp(
-          title: appName,
-          debugShowCheckedModeBanner: runtime == "Test",
-          scaffoldMessengerKey: _scaffoldKey,
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          // themeMode: ui.themeMode,
-          initialRoute: SplashScreen.routeName,
-          navigatorKey: locator<NavigationService> ().navigatorKey,
-          builder: (cx, child) {
-            return ScrollConfiguration(
-              behavior: ScrollClean ().copyWith(
-                scrollbars: false
-              ), 
-              child: child ?? Container ()
-            );
-          },
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          // locale: Locale (ui.locale),
-          onGenerateRoute: (settings) => RouterAdmin.generateRoute (
-            context, settings
-          ),
-          navigatorObservers: [
-            locator<RouteObserver> ()
-          ],
-        ),
       ),
     );
   }
