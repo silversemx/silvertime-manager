dcc62df766868c22ca5b1e76db2b54d4666a4719
Djcharles26 <jlaraanaya@gmail.com>
2023-04-02 21:53:15 -0600

    Updated main layout with navbar

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,4 +1,5 @@
 import 'package:silvertime/include.dart';
+import 'package:silvertime/widgets/common/navbar.dart';
 
 class MainLayout extends StatefulWidget {
   final Widget child;
@@ -9,8 +10,58 @@ class MainLayout extends StatefulWidget {
 }
 
 class _MainLayoutState extends State<MainLayout> {
+  final ScrollController _scrollController = ScrollController();
+  bool changeColor = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if(
+        _scrollController.hasClients && _scrollController.offset > kToolbarHeight &&
+        !changeColor
+      ) {
+        setState(() {
+          changeColor = true;
+        });
+      } else if (
+        _scrollController.hasClients && _scrollController.offset <= kToolbarHeight
+      ) {
+        setState(() {
+          changeColor = false;
+        });
+      }
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: Navbar (
+        changeColor: changeColor,
+      ),
+      body: Scrollbar (
+        controller: _scrollController,
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Container (
+            constraints: BoxConstraints (
+              minHeight: MediaQuery.of(context).size.height
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.15
+            ),
+            child: Column (
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                widget.child
+              ],
+            )
+          ),
+        ),
+      ),
+    );
   }
 }
