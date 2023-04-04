2ef472cc208ac93e220092b2b1ea987de5c8f978
Djcharles26 <jlaraanaya@gmail.com>
2023-04-04 00:37:08 -0600

    Added success and error colors

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -5,7 +5,13 @@ class UIColors {
   
   static const Color white = Color (0xFFF9FBFC);
 
+  static const Color lightBackground = Color (0XFFEFF4FF);
+
   static const Color primary = Color (0xFF34475C);
 
   static const Color hint = Color.fromARGB(105, 52, 71, 92);
+  
+  static const Color error = Color.fromRGBO(255, 54, 54, 1.0);
+
+  static const Color inputSuccess = Color.fromARGB(255, 15, 154, 0);
 }
