009bc9bf9589a993576c4f9ef43929853c1913d9
Djcharles26 <jlaraanaya@gmail.com>
2023-04-04 00:37:40 -0600

    Updated home screen with stats and welcome

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:silvertime/include.dart';
+import 'package:silvertime/style/container.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -45,6 +47,176 @@ class _HomeScreenState extends State<HomeScreen> {
       ],
     );
   }
+
+  Widget _overview (String title, IconData icon, {required Widget child}) {
+    return Container (
+      constraints: BoxConstraints (
+        maxWidth: constrainedBigWidth(
+          context, MediaQuery.of(context).size.width * 0.37,
+          constraintWidth: 300
+        )
+      ),
+      decoration: containerDecoration,
+      padding: const EdgeInsets.all(16),
+      child: Column (
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Wrap(
+            alignment: WrapAlignment.start,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Text (
+                title,
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              const SizedBox(width: 16),
+              Icon (
+                icon,
+                size: 24,
+                color: UIColors.hint,
+              ),
+            ],
+          ),
+          const Divider (
+            endIndent: 35,
+            thickness: 0.1,
+          ),
+          const SizedBox(height: 16),
+          child
+        ],
+      ),
+    );
+  }
+
+  Widget _accessData ({
+    required num daily, required num monthly, required num weekly, required num users
+  }) {
+    return Column (
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        RichText(
+          text: TextSpan (
+            text: S.of(context).dailyAccess,
+            style: Theme.of(context).textTheme.headline3,
+            children: [
+              const TextSpan (
+                text: "  "
+              ),
+              TextSpan (
+                text: daily.toStringAsFixed(0),
+                style: Theme.of(context).textTheme.bodyText1
+              )
+            ]
+          )
+        ),
+        const SizedBox(height: 16),
+        RichText(
+          text: TextSpan (
+            text: S.of(context).weeklyAccess,
+            style: Theme.of(context).textTheme.headline3,
+            children: [
+              const TextSpan (
+                text: "  "
+              ),
+              TextSpan (
+                text: weekly.toStringAsFixed(0),
+                style: Theme.of(context).textTheme.bodyText1
+              )
+            ]
+          )
+        ),
+        const SizedBox(height: 16),
+        RichText(
+          text: TextSpan (
+            text: S.of(context).monthlyAccess,
+            style: Theme.of(context).textTheme.headline3,
+            children: [
+              const TextSpan (
+                text: "  "
+              ),
+              TextSpan (
+                text: monthly.toStringAsFixed(0),
+                style: Theme.of(context).textTheme.bodyText1
+              )
+            ]
+          )
+        ),
+        const SizedBox(height: 16),
+        RichText(
+          text: TextSpan (
+            text: S.of(context).activeUsers,
+            style: Theme.of(context).textTheme.headline3,
+            children: [
+              const TextSpan (
+                text: "  "
+              ),
+              TextSpan (
+                text: users.toStringAsFixed(0),
+                style: Theme.of(context).textTheme.bodyText1
+              )
+            ]
+          )
+        )
+      ],
+    );
+  }
+
+  Widget _webOverview () {
+    return _overview(
+      S.of(context).web, 
+      FontAwesomeIcons.desktop, 
+      child: Column (
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _accessData(daily: 170, monthly: 1500, weekly: 6225, users: 57)
+        ],
+      )
+    );
+  }
+
+  Widget _mobileOverview () {
+    return _overview(
+      S.of(context).mobile, 
+      FontAwesomeIcons.mobile, 
+      child: Column (
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _accessData(daily: 280, monthly: 3500, weekly: 9225, users: 172)
+        ],
+      )
+    );
+  }  
+
+  Widget _platformOverview () {
+    return Column (
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text (
+          S.of(context).platformOverview,
+          style: Theme.of(context).textTheme.headline2,
+        ),
+        const SizedBox(height: 16),
+        SizedBox (
+          width: double.infinity,
+          child: Wrap (
+            alignment: WrapAlignment.spaceBetween,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            spacing: 16,
+            runSpacing: 48,
+            children: [
+              _webOverview (),
+              _mobileOverview()
+            ],
+          ),
+        )
+      ],
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -59,6 +231,11 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           const SizedBox(height: 32),
           _title (),
+          const Divider (
+            height: 48,
+          ),
+          const SizedBox(height: 16),
+          _platformOverview ()
         ],
       ),
     );
