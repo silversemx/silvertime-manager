ed75ed968289b1eb0772688a00e2fce2736b69f7
Djcharles26 <jlaraanaya@gmail.com>
2023-04-03 18:54:05 -0600

    Updated navbar with light and collapsed data

--- lib/widgets/common/navbar.dart
+++ lib/widgets/common/navbar.dart
@@ -1,9 +1,12 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:silvertime/include.dart';
 import 'package:silvertime/style/colors.dart';
 
 class Navbar extends StatelessWidget  implements PreferredSizeWidget {
   final bool changeColor;
   const Navbar({Key? key, this.changeColor = false}): super (key: key);
+
+  static const double navbarHeight = kToolbarHeight + 16;
 
   Widget _label (BuildContext context, String text, String route) {
     return TextButton (
@@ -14,68 +17,140 @@ class Navbar extends StatelessWidget  implements PreferredSizeWidget {
       child: Text (
         text.toUpperCase(),
         style: Theme.of(context).textTheme.headline3!.copyWith(
-          color: changeColor ? Theme.of(context).backgroundColor : null
+          color: changeColor ? UIColors.white : null
         ),
       ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      height: kToolbarHeight + 16,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 32
-      ),
-      decoration: BoxDecoration (
-        color: changeColor 
-        ? UIColors.primary
-        : Theme.of(context).backgroundColor,
-        borderRadius: const BorderRadius.only  (
-          bottomLeft: Radius.circular(24),
-          bottomRight: Radius.circular(24)
-        )
-      ),
-      child: Row (
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row (
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Hero(
-                tag: "logo",
-                child: Image.asset (
-                  changeColor
-                  ? "assets/logos/silvertime_white0.5.png"
-                  : "assets/logos/silvertime0.5.png",
-                  height: kToolbarHeight - 8,
-                  filterQuality: FilterQuality.high,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              const SizedBox(width: 16),
-              _label (context, S.of(context).home, "/home"),
-              _label (context, S.of(context).users, "/users"),
-              _label (context, S.of(context).resources, "/resources"),
-            ],
+  Widget _lightWidth (BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: SizedBox(
+        width: 32,
+        child: DropdownButtonFormField2(
+          onChanged: (_){},
+          customButton:  Icon (
+            Icons.menu,
+            color: changeColor 
+            ? UIColors.white
+            : UIColors.primary
           ),
-          Row (
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _label (context, S.of(context).yourAccount, "/account"),
-            ],
-          )
-        ],
+          dropdownStyleData: DropdownStyleData(
+            maxHeight: 200,
+            width: MediaQuery.of(context).size.width,
+            padding: null,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only (
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12)
+              ),
+              color: changeColor
+              ? UIColors.primary
+              : UIColors.white
+            ),
+            elevation: 0,
+            offset: const Offset(-20, 0),
+            scrollbarTheme: ScrollbarThemeData(
+              radius: const Radius.circular(40),
+              thickness: MaterialStateProperty.all(6),
+              thumbVisibility: MaterialStateProperty.all(true),
+            )
+          ),
+          decoration: const InputDecoration (
+            border: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            errorBorder: InputBorder.none
+          ),
+          items: [
+            DropdownMenuItem<String> (
+              value: "/home",
+              alignment: Alignment.centerLeft,
+              child: _label(context, S.of(context).home, "/home"),
+            ),
+            DropdownMenuItem<String> (
+              value: "/users",
+              alignment: Alignment.centerLeft,
+              child: _label(context, S.of(context).users, "/users"),
+            ),
+            DropdownMenuItem<String> (
+              value: "/resources",
+              alignment: Alignment.centerLeft,
+              child: _label(context, S.of(context).resources, "/resources"),
+            )
+          ]
+        ),
       ),
     );
   }
 
+  Widget _normalWidth (BuildContext context) {
+    return Row (
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row (
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Hero(
+              tag: "logo",
+              child: Image.asset (
+                changeColor
+                ? "assets/logos/silvertime_white0.5.png"
+                : "assets/logos/silvertime0.5.png",
+                height: kToolbarHeight - 8,
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(width: 16),
+            _label (context, S.of(context).home, "/home"),
+            _label (context, S.of(context).users, "/users"),
+            _label (context, S.of(context).resources, "/resources"),
+          ],
+        ),
+        Row (
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _label (context, S.of(context).yourAccount, "/account"),
+          ],
+        )
+      ],
+    );
+  }
+
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 16);
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          height: kToolbarHeight + 16,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32
+          ),
+          decoration: BoxDecoration (
+            color: changeColor 
+            ? UIColors.primary
+            : Theme.of(context).backgroundColor,
+            borderRadius: const BorderRadius.only  (
+              bottomLeft: Radius.circular(24),
+              bottomRight: Radius.circular(24)
+            )
+          ),
+          child: constraints.maxWidth < 660
+          ? _lightWidth (context)
+          : _normalWidth (context),
+        );
+      }
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(navbarHeight);
 }
