e4d6b28e3afe959369ef2f8c2b17cc88838997bc
Djcharles26 <jlaraanaya@gmail.com>
2023-04-05 00:07:12 -0600

    Added service base screen

---
+++ lib/screens/service.dart
@@ -0,0 +1,16 @@
+import 'package:silvertime/include.dart';
+
+class ServiceScreen extends StatefulWidget {
+  const ServiceScreen({super.key});
+  static const String routeName = "/resources/service";
+
+  @override
+  State<ServiceScreen> createState() => _ServiceScreenState();
+}
+
+class _ServiceScreenState extends State<ServiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
