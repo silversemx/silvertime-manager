bf4a85042385a9d8fcc3a9ba548360172e2e0abe
Djcharles26 <jlaraanaya@gmail.com>
2023-04-11 11:32:41 -0600

    Removed disks from resource types

--- lib/models/resources/types.dart
+++ lib/models/resources/types.dart
@@ -5,7 +5,6 @@ enum ResourceType {
   none,
   services,
   serviceTags,
-  disks,
   networks,
   machines,
   // storages
@@ -20,14 +19,34 @@ extension ResourceTypeExt on ResourceType {
         return S.of(context).services;
       case ResourceType.serviceTags:
         return S.of(context).serviceTags;
-      case ResourceType.disks:
-        return S.of(context).disks;
       case ResourceType.networks:
         return S.of(context).networks;
       case ResourceType.machines:
         return S.of(context).machines;
       // case ResourceType.storages:
       //   return S.of(context).storages;
+    }
+  }
+}
+
+enum ResourceServiceType {
+  none,
+  instances,
+  actions,
+  options
+}
+
+extension ResourceServiceTypeExt on ResourceServiceType {
+  String name (BuildContext context) {
+    switch (this) {
+      case ResourceServiceType.none:
+        return S.of(context).selectOne;
+      case ResourceServiceType.instances:
+        return S.of(context).instances;
+      case ResourceServiceType.actions:
+        return S.of(context).actions;
+      case ResourceServiceType.options:
+        return S.of(context).options;
     }
   }
 }
