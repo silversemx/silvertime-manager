86a06eb8b9ae98167b9dee27a7393af794944d66
Djcharles26 <jlaraanaya@gmail.com>
2023-04-10 20:00:58 -0600

    Added tag data & dialog widgets

---
+++ lib/widgets/resources/services/tags/tag_data.dart
@@ -0,0 +1,78 @@
+import 'package:provider/provider.dart';
+import 'package:silvertime/include.dart';
+import 'package:silvertime/models/resources/service/service_tag.dart';
+import 'package:silvertime/models/resources/types.dart';
+import 'package:silvertime/providers/resources/services/tags.dart';
+import 'package:silvertime/widgets/custom_table_full_data.dart';
+import 'package:silvertime/widgets/resources/services/tags/tag_dialog.dart';
+
+class ServiceTagData extends StatelessWidget {
+  const ServiceTagData({super.key});
+
+  List<String> columns (BuildContext context) => [
+    "Id", 
+    S.of(context).name, 
+    S.of(context).value
+  ];
+
+  List<DataCell> cells (BuildContext context, ServiceTag serviceTag) {
+    return [
+      DataCell(
+        Center (
+          child: SelectableText (
+            serviceTag.id,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        )
+      ),
+      DataCell(
+        Center (
+          child: Tooltip(
+            message: serviceTag.description,
+            waitDuration: Duration.zero,
+            child: SelectableText (
+              serviceTag.name,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+        )
+      ),
+      DataCell(
+        Center (
+          child: SelectableText (
+            serviceTag.value.toString(),
+            style: Theme.of(context).textTheme.headline3!.copyWith(
+              color: UIColors.error
+            ),
+          ),
+        )
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ServiceTags>(
+      builder: (context, serviceTags, _) {
+        return ResourceDataWidget<ServiceTag>(
+          title: ResourceType.serviceTags.name(context),
+          fetch: serviceTags.getServiceTags, 
+          dismiss: serviceTags.dismiss,
+          createSuccessfullyText: S.of(context).serviceTagSuccessfullyCreated, 
+          updateSuccessfullyText: S.of(context).serviceTagSuccessfullyUpdated, 
+          deleteSuccessfullyText: S.of(context).serviceTagSuccessfullyDeleted, 
+          dialogCall: ([ServiceTag? serviceTag]) {
+            return ServiceTagDialog (serviceTag: serviceTag);
+          }, 
+          id: (serviceTag) => serviceTag.id,
+          deleteDataStream: serviceTags.removeServiceTags, 
+          deleteProgressDialogTitle: S.of(context).deletingServiceTags, 
+          data: serviceTags.tags, 
+          cells: (serviceTag) => cells (context, serviceTag), 
+          columns: columns(context),
+          pages: serviceTags.pages
+        );
+      }
+    );
+  }
+}
---
+++ lib/widgets/resources/services/tags/tag_dialog.dart
@@ -0,0 +1,137 @@
+import 'package:http_request_utils/models/http_exception.dart';
+import 'package:provider/provider.dart';
+import 'package:silvertime/include.dart';
+import 'package:silvertime/models/resources/service/service_tag.dart';
+import 'package:silvertime/providers/resources/services/tags.dart';
+import 'package:silvertime/widgets/in_app_messages/error_dialog.dart';
+import 'package:silvertime/widgets/inputs/custom_input_field.dart';
+import 'package:silvertime/widgets/utils/confirm_row.dart';
+
+class ServiceTagDialog extends StatefulWidget {
+  final ServiceTag? serviceTag;
+  const ServiceTagDialog({super.key, this.serviceTag});
+
+  @override
+  State<ServiceTagDialog> createState() => _ServiceTagDialogState();
+}
+
+class _ServiceTagDialogState extends State<ServiceTagDialog> {
+  late ServiceTag serviceTag;
+  bool _saving = false;
+  Map<String, bool> validation = {};
+  final _formKey = GlobalKey<FormState> ();
+  final ScrollController _scrollController = ScrollController();
+  
+  @override
+  void initState() {
+    super.initState();
+    serviceTag = widget.serviceTag ?? ServiceTag.empty ();
+  }
+
+  void _save () async {
+    bool formValidation = _formKey.currentState!.validate();
+    setState(() {
+      validation = serviceTag.isComplete();
+    });
+
+    if (validation ["total"]! && formValidation) {
+      setState(() {
+        _saving = true;
+      });
+      try { 
+        if (widget.serviceTag == null) {
+          await Provider.of<ServiceTags> (context, listen: false).createServiceTag(
+            serviceTag
+          );
+        } else {
+          await Provider.of<ServiceTags> (context, listen: false).updateServiceTag (
+            widget.serviceTag!.id, serviceTag
+          );
+        }
+
+        Navigator.of(context).pop (true);
+      } on HttpException catch (error) {
+        showErrorDialog(context, exception: error);
+      } finally {
+        if (mounted) {
+          setState(() {
+            _saving = false;
+          });
+        }
+      }
+    }
+  }
+
+  Widget _form () {
+    return Form (
+      key: _formKey,
+      child: Column (
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomInputField (
+            initialValue: serviceTag.name,
+            label: S.of(context).name,
+            type: TextInputType.text,
+            action: TextInputAction.next,
+            onChanged: (val) {
+              serviceTag.name = val;
+            },
+            requiredValue: true,
+            validation: validation ["name"],
+          ),
+          CustomInputField (
+            initialValue: serviceTag.description,
+            label: S.of(context).description,
+            type: TextInputType.text,
+            action: TextInputAction.next,
+            onChanged: (val) {
+              serviceTag.description = val;
+            },
+            requiredValue: true,
+            validation: validation ["description"],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      insetPadding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.2,
+      ),
+      child: Scrollbar(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Container (
+            padding: const EdgeInsets.all(16),
+            child: Column (
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text (
+                  widget.serviceTag == null
+                  ? S.of(context).createServiceTag
+                  : S.of(context).editServiceTag,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+                const SizedBox(height: 16),
+                _form (),
+                const SizedBox(height: 16),
+                ConfirmRow (
+                  onPressedOkay: _save,
+                  onPressedCancel: Navigator.of(context).pop,
+                  okayLoading: _saving,
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
