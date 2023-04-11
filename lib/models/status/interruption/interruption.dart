79c09ec06ae811d25314c907f35b9c0bc65c8d6d
Djcharles26 <jlaraanaya@gmail.com>
2023-04-11 11:32:25 -0600

    Added title in interruption model

--- lib/models/status/interruption/interruption.dart
+++ lib/models/status/interruption/interruption.dart
@@ -1,12 +1,18 @@
 import 'package:http_request_utils/body_utils.dart';
 import 'package:silvertime/include.dart';
-import 'package:silvertime/models/status/interruption/interruption_status.dart';
+import 'package:silvertime/models/status/interruption/interruption_types.dart';
 import 'package:silvertime/models/system/types.dart';
+
+export 'package:silvertime/models/status/interruption/interruption_types.dart';
+export 'package:silvertime/models/system/types.dart';
 
 class Interruption {
   String id = "";
+  String title = "";
   String? service;
-  String? serviceInstance;
+  String? serviceName;
+  String? instance;
+  String? instanceName;
   ExecutionScope scope = ExecutionScope.none;
   ExecutionType execution = ExecutionType.none;
   ExecutionExit exit = ExecutionExit.none;
@@ -15,11 +21,12 @@ class Interruption {
   Duration duration = Duration.zero;
   InterruptionStatus status = InterruptionStatus.none;
   String description = "";
-  String solution = "";
+  String? solution;
   DateTime date = DateTime.now ();
 
   Interruption ({
     required this.id,
+    required this.title,
     required this.scope,
     required this.execution,
     required this.exit,
@@ -27,10 +34,12 @@ class Interruption {
     required this.duration,
     required this.status,
     required this.description,
-    required this.solution,
     required this.date,
     this.service,
-    this.serviceInstance,
+    this.serviceName,
+    this.instance,
+    this.instanceName,
+    this.solution,
     this.end,
   });
   
@@ -39,11 +48,12 @@ class Interruption {
   factory Interruption.fromJson (dynamic json) {
     return Interruption (
       id: jsonField<String> (json, ["_id", "\$oid"], nullable: false),
+      title: jsonField<String> (json, ["title",],  nullable: false),
       scope: ExecutionScope.values[
         jsonField<int> (json, ["scope",], defaultValue: 0) 
       ],
       execution: ExecutionType.values[
-        jsonField<int> (json, ["",], defaultValue: 0) 
+        jsonField<int> (json, ["execution"], defaultValue: 0) 
       ],
       exit: ExecutionExit.values[
         jsonField<int> (json, ["exit",], defaultValue: 0) 
@@ -60,13 +70,15 @@ class Interruption {
         jsonField<int> (json, ["status",], defaultValue: 0) 
       ],
       description: jsonField<String> (json, ["description",],  nullable: false),
-      solution: jsonField<String> (json, ["solution",],  nullable: false),
+      solution: jsonField<String> (json, ["solution",]),
       date: dateTimefromMillisecondsNoZero(
         jsonField<int> (json, ["date", "\$date"]),
         defaultNow: true
       )!,
-      service: jsonField<String> (json, ["service", "\$oid"]),
-      serviceInstance: jsonField<String> (json, ["service_instance", "\$oid"]),
+      service: jsonField<String> (json, ["service", "_id", "\$oid"]),
+      serviceName: jsonField<String> (json, ["service", "name",]),
+      instance: jsonField<String> (json, ["service_instance", "_id", "\$oid"]),
+      instanceName: jsonField<String> (json, ["service_instance", "name",]),
       end: dateTimefromMillisecondsNoZero(
         jsonField<int> (json, ["date", "\$date"]),
       )
@@ -74,39 +86,48 @@ class Interruption {
   }
 
   Map<String, bool> isComplete () {
+    bool title = this.title.isNotEmpty;
     bool scope = this.scope != ExecutionScope.none;
-    bool execution = this.execution != ExecutionScope.none;
+    bool execution = this.execution != ExecutionType.none;
     bool exit = this.exit != ExecutionExit.none;
     bool description = this.description.isNotEmpty;
-    bool solution = this.solution.isNotEmpty;
+    bool status = this.status != InterruptionStatus.none;
+    bool solution =  this.status != InterruptionStatus.solved  || (
+      this.solution?.isNotEmpty ?? false
+    );
     bool service = this.scope == ExecutionScope.global || (
       this.service?.isNotEmpty ?? false
     );
-    bool serviceInstance = this.scope != ExecutionScope.instance || (
-      this.serviceInstance?.isNotEmpty ?? false
+    bool instance = this.scope != ExecutionScope.instance || (
+      this.instance?.isNotEmpty ?? false
     );
 
     return {
+      "title": !title,
       "scope": !scope,
       "execution": !execution,
       "exit": !exit,
       "description": !description,
+      "status": !status,
       "solution": !solution,
       "service": !service,
-      "serviceInstance": !serviceInstance,
-      "total": scope &&
+      "instance": !instance,
+      "total": title &&
+        scope &&
         execution &&
         exit &&
         description &&
+        status &&
         solution &&
         service &&
-        serviceInstance
+        instance
     };
   }
 
   Map<String, dynamic> toJson () => {
+    "title": title,
     "service": service,
-    "serviceInstance": serviceInstance,
+    "instance": instance,
     "scope": scope.index,
     "execution": execution.index,
     "exit": exit.index,
@@ -114,6 +135,7 @@ class Interruption {
     "end": end?.millisecondsSinceEpoch,
     "duration": duration.inMilliseconds,
     "description": description,
+    "status": status.index,
     "solution": solution,
   };
 }
