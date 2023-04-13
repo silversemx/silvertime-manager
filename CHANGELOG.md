a9f790497eec2b61d4ead0d085e0dd4516844544
Djcharles26 <jlaraanaya@gmail.com>
2023-04-12 21:16:49 -0600

    Updated version to 1.0 ⬆️

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Version 1.0
+- Added users management
+- Added resources management
+- Added interruptions management
+- Added maintenances management
+- Added full data configuration
---
+++ lib/version.dart
@@ -0,0 +1,4 @@
+const String version = "1.0";
+const String versionName = "Release 1.0";
+const String versionAuthor = "Silverse - Juan Carlos Lara";
+const String versionDate = "12/04/2023";
